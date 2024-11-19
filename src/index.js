ef4c2544da6830a7b01d988a7750258bfced09a7
Mohammad Meraj Ansari <mohammadmerajansari352@gmail.com>
2024-11-20 00:14:17 +0530

    final push

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import Navbar from './components/Navbar';
+import Header from './components/Header';
+import About from './components/About';
+import Projects from './components/Projects';
+import Testimonials from './components/Testimonials';
+// import Contact from './components/Contact';
+import Contact from './components/Contact'
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Footer from './components/Footer';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <BrowserRouter>
+  {/* <Navbar/> */}
+  <ToastContainer/>
+<Header />
+<About/>
+<Projects />
+<Testimonials/>
+<Contact/>
+<Footer/>
+</BrowserRouter>
+);
